e4584ec296718db3447d4707679b0c3dc9fbd8b8
Tiniboi <137842262+nikici4re@users.noreply.github.com>
2023-07-10 12:32:31 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord 
+from discord.ext import commands 
+from discord.ext.commands import application_commands 
+
+
+@bot.event
+async def on_ready():
+  print("supportive online")
+
+
+
+
+
+bot.run('tokengoeshere')
